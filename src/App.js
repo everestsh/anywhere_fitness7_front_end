06c7236ebd3d43d91828781d8115964ccbb825ba
sooof <sooof@me.com>
2021-11-22 01:47:23 -0800

    add Home

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import './App.css';
-import { BrowserRouter as Router, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import React from 'react';
 
 import Header from './components/Header';
 import Footer from './components/Footer';
+import Home from './components/Home';
 
 function App() {
   return (
   <Router>
     <Header />
     <Switch>
-    <h1>Test</h1>
+      <Route exact path="/" component={Home}/>
     </Switch>
     <Footer />
   </Router>
