7b22d1be17dda5e5857b997873a881c7558903f7
sooof <sooof@me.com>
2021-11-24 13:05:51 -0800

    Login use axios post

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,10 +1,42 @@
-import React from 'react'
-import {Link} from 'react-router-dom'
+
+import React, { useState } from 'react'
+import { useHistory, Link } from 'react-router-dom';
+import axios from 'axios';
 
 import homeImg from '../assets/fitness3.jpg'
 import '../CSS/Login.css';
 
 const Login = () =>{
+    const initusername ={
+        username:'',
+        password:''
+    }
+    const [user, setUser] = useState(initusername)
+    const { push } = useHistory();
+
+    const handleChange = (e) => {
+        setUser({
+            ...user,
+            [e.target.name]: e.target.value
+        })
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        // push('/classes');
+        axios.post(`https://ft-anywherefitness-7.herokuapp.com/api/auth/login`, user)  
+        .then(resp => {                    
+            console.log('resp in Login: ', resp);
+            localStorage.setItem('token', resp.data.token);                    
+            localStorage.setItem('role', resp.data.role);
+            localStorage.setItem('message', resp.data.message);
+            // props.setIsLoggedIn(localStorage.getItem('token'));      
+            push(`/classes`);                 
+        })
+        .catch(err => {
+            console.log(err);                
+        })
+    }
 
     return(
         <div className="login-from">
@@ -15,16 +47,14 @@ const Login = () =>{
                     </div>
                     {/** Username */}
                     <div className="login-form-group from-username">
-                        {/* <div className="label-title"> */}
                             <h3>Username:</h3>
-                        {/* </div> */}
                         <label className="login-label-group">
                             <input
                             type='text'
                             name='username'
-                            // value={user.username}
-                            // placeholder = 'Please enter a username'
-                            // onChange={handleChange}
+                            value={user.username}
+                            placeholder = 'Please enter a username'
+                            onChange={handleChange}
                             />
                         </label>
 
@@ -32,31 +62,22 @@ const Login = () =>{
 
                     {/** Password */}
                     <div className="login-form-group from-password">
-                        {/* <div className="label-title"> */}
                             <h3>Password:</h3>
-                        {/* </div> */}
 
                         <label className="login-label-group">
                             <input
                             type='text'
                             name='password'
-                            // value={user.password}
-                            // placeholder="Enter your password"
-                            // onChange={handleChange}
+                            value={user.password}
+                            placeholder="Enter your password"
+                            onChange={handleChange}
                             />
                         </label> 
                     </div>
-
-                    {/** Line */}
-                    <div className="login-form-group from-role">
-                        {/* <div className="label-title"> */}
-                            {/* <h3>Role:</h3> */}
-                        {/* </div> */}
-                    </div>         
-
+ 
                     {/* Submit */}
                         <div >
-                            <button  className='login-submit-button' >Login</button>
+                            <button onClick={handleSubmit} className='login-submit-button' >Login</button>
                         </div>
                 </form>
             </div>
