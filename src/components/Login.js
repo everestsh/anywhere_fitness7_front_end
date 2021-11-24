024ebb4f253a16f9ba5f9c2c4912ca5b230c63b5
sooof <sooof@me.com>
2021-11-23 23:02:49 -0800

    add Login.js Login.css

---
+++ src/components/Login.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import {Link} from 'react-router-dom'
+
+import homeImg from '../assets/fitness3.jpg'
+import '../CSS/Home.css';
+
+const Login = () =>{
+
+    return(
+            <div className="login-container">
+                <h1>Login</h1>
+            </div>
+    );
+}
+
+export default Login;
