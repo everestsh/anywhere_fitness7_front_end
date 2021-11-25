08b423fd844b112de7b364ee96538cc089b457f7
sooof <sooof@me.com>
2021-11-24 17:03:53 -0800

    get data for Classes.js

--- src/components/Classes.js
+++ src/components/Classes.js
@@ -1,10 +1,22 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 import {Link} from 'react-router-dom'
 
-
-
+import axios from 'axios';
 
 const Classes = () =>{
+
+    useEffect(() => {
+        axios
+        .get('https://ft-anywherefitness-7.herokuapp.com/api/classes')
+        .then(res => {
+            console.log(res);
+            // setAllClasses(res.data);
+        })
+        .catch(err => {
+            console.error(err);
+        })
+    
+    }, []);
 
     return(
             <div className="home-container">
